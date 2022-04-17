28ae1aba2e627d61ce4b3e98e11ab535f5a3bf5b
aravindthonupunuri <aravindthonupunuri75@gmail.com>
2022-04-16 21:16:37 -0700

    error handling

--- backend/src/main/java/com/assassins/assassin/customer/CustomerController.java
+++ backend/src/main/java/com/assassins/assassin/customer/CustomerController.java
@@ -2,6 +2,8 @@ package com.assassins.assassin.customer;
 
 import com.assassins.assassin.models.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.NoSuchAlgorithmException;
@@ -23,12 +25,25 @@ public class CustomerController {
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public void registerCustomer(@RequestBody Customer customer) throws NoSuchAlgorithmException {
-        customerService.register(customer);
+    public ResponseEntity<String> registerCustomer(@RequestBody Customer customer) throws NoSuchAlgorithmException {
+        try {
+            customerService.register(customer);
+            return ResponseEntity.status(HttpStatus.OK).body("success");
+        }
+        catch (Exception e) {
+            System.out.println("error while registering");
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("invalid credentials");
+        }
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public void loginCustomer(@RequestBody Customer customer) throws NoSuchAlgorithmException {
-        customerService.login(customer);
+    public ResponseEntity<String> loginCustomer(@RequestBody Customer customer) throws NoSuchAlgorithmException {
+        try {
+            customerService.login(customer);
+            return ResponseEntity.status(HttpStatus.OK).body("success");
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("invalid credentials");
+        }
     }
 }

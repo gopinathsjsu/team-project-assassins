8e2b9a9cbd7d617f0d76ec298fc7381c1d85ab27
Harshavardhan Padidala <v-hapadi@microsoft.com>
2022-04-26 23:55:24 -0700

    Booking dto and repository added

---
+++ backend/src/main/java/com/assassins/assassin/booking/BookingRepository.java
@@ -0,0 +1,20 @@
+package com.assassins.assassin.customer;
+
+import com.assassins.assassin.models.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Integer> {
+
+    @Query(value = "SELECT * FROM booking b where b.room_id = :roomId", nativeQuery = true)
+    List<Booking> fetchBookingsByRoomId(@Param("roomId")int roomId);
+
+    @Query(value = "SELECT * FROM booking b where b.room_id = :roomId and b.checkin <= :date and b.checkout >= :date", nativeQuery = true)
+    List<Booking> fetchBookingsByRoomIdAndDate(@Param("roomId")int roomId, @Param("date") LocalDate date);
+}

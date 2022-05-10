8ef465b9692f9d2075515ee2f555f3f5450d6f48
ramasuryadevara <suryadevararama123@gmail.com>
2022-05-10 21:42:00 +0000

    handling price event

--- frontend/src/components/UserPages/Bookingscreen.js
+++ frontend/src/components/UserPages/Bookingscreen.js
@@ -59,3 +59,87 @@ const handleUseReedem = () => {
     setTotalAmount(updatedCostAfterRewards);
   }
 };
+//handling price event
+const handlePrice = async () => {
+  const fromDate = moment(match.params.fromdate, "DD-MM-YYYY");
+  const toDate = moment(match.params.todate, "DD-MM-YYYY");
+  console.log("@@", fromDate._i, totalAmount, amenitiesAmount);
+  const bookingDetails = {
+    room,
+    userid: JSON.parse(localStorage.getItem("currentUserId")),
+    fromdate: fromDate._i,
+    todate: toDate._i,
+    guestCount,
+    totalamount: totalAmount + amenitiesAmount,
+    totaldays: totalDays,
+    remainingAmount: room.rentperday,
+    extracostapplied: "",
+    offerapplied: "",
+  };
+
+  // console.log(JSON.stringify(bookingDetails))
+  console.log(bookingDetails);
+
+  axios
+    .post("http://localhost:8080/api/bookings/getUpdatedPrice", bookingDetails)
+    .then((result) => {
+      console.log(result);
+      console.log(typeof ((guestCount - 2) * 15));
+      console.log(typeof result.data.body.totalAmount);
+
+      var newRoomCost = 0;
+      if (guestCount > 2) {
+        newRoomCost =
+          parseInt(result.data.body.totalAmount) + (guestCount - 2) * 15;
+        if (roomCount > 1) {
+          newRoomCost = newRoomCost * roomCount;
+        }
+      } else {
+        newRoomCost = result.data.body.totalAmount;
+        if (roomCount > 1) {
+          newRoomCost = newRoomCost * roomCount;
+        }
+      }
+      console.log(newRoomCost);
+      setnewtotal(newRoomCost);
+      setnewOffer(result.data.body.offerapplied);
+      setExtra(result.data.body.extracostapplied);
+      setDisplay(true);
+      // setUpdateUserRewarrds(true);
+      if (result.data.body.offerapplied !== "") {
+        console.log(
+          "====================================== RP" +
+            Number(JSON.parse(localStorage.getItem("rewards"))) +
+            5
+        );
+        // console.log(JSON.parse(localStorage.getItem("currentUser")).rewards);
+        // const result = axios
+        //   .put(
+        //     "http://localhost:8080/api/users/updateUserRewards/" +
+        //       JSON.parse(localStorage.getItem("currentUserId")),
+        //     {
+        //       rewardsPoints:
+        //         Number(
+        //           JSON.parse(localStorage.getItem("rewards"))
+        //         ) + 5,
+        //     }
+        //   )
+        //   .then((userRes) => {
+        //     setIsRoomBooked(true);
+        //     console.log(userRes);
+        //     let rewards =
+        //       Number(
+        //         JSON.parse(localStorage.getItem("rewards"))
+        //       ) + 5;
+        //       //
+        //     localStorage.setItem("rewards", JSON.stringify(rewards));
+        //   })
+        //   .catch((error) => {
+        //     console.log(error);
+        //   });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
